2436c484f710bff056d773571552159e16d0743b
João Vitor S. G <50921035+JojiAbel@users.noreply.github.com>
2020-02-05 16:43:36 -0300

    Fix

--- src/requests/company/DeleteOneJob.js
+++ src/requests/company/DeleteOneJob.js
@@ -1,7 +1,7 @@
 import api from '../../lib/assets/const/baseUrl';
 
-const DeleteOneJob  = async (idCompany) => {
-    const res = await api.delete(`/vagas/${idCompany}`)
+const DeleteOneJob  = async (idJob) => {
+    const res = await api.delete(`/vagas/${idJob}`)
     try {
         return console.log(res);
     } catch (err) {
